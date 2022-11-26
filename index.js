0f2e0e6c307ce7ccc778365a78b902ec6ceefd6a
DianaRomanova <romanowadia@gmail.com>
2022-11-26 16:58:24 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+//console.log(1 == 1);
+//console.log(1 == 2);
+//console.log(2 !== "4");
+//console.log(8 > 6);
+
+//const size = 38;
+//console.log(size < 40 && size > 34);
+
+const weight = 10;
+console.log(weight > 3 || weight < 6);
